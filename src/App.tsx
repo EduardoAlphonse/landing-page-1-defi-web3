d275e074d30a4d0138b39bd6738d366e4cc82df9
EduardoAlphonse <eduardoliveira.dev@gmail.com>
2023-01-03 11:28:24 -0300

    first commit and SVGR configuration

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ReactComponent as Avax } from './assets/avax.svg';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <Avax className="text-green-500" />
+      </header>
+    </main>
+  );
+}
+
+export default App;
